f0b84bdcca8b53d601e01132d0ce48a57df5151c
Maureen Barahona <maureenbarahona@gmail.com>
2020-10-31 15:13:16 -0600

    Inicio fig 12_19 y 12_20

--- src/main/java/hn/mau/cap12/fig12_19/PruebaBotonOpcion.java
+++ src/main/java/hn/mau/cap12/fig12_19/PruebaBotonOpcion.java
@@ -16,7 +16,7 @@ public class PruebaBotonOpcion {
     public static void main(String[] args) {
         MarcoBotonOpcion marcoBotonOpcion = new MarcoBotonOpcion();
         marcoBotonOpcion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        marcoBotonOpcion.setSize(300, 100);
+        marcoBotonOpcion.setSize(400, 100);
         marcoBotonOpcion.setVisible(true);
     }
 }
