e8512acf34dc501d7d2afc8d6852094a476d3e28
Maureen Barahona <maureenbarahona@gmail.com>
2020-10-31 13:46:33 -0600

    obtiene la entrada del usuario de los diálogos de entrada de JOptionPane

---
+++ src/main/java/hn/mau/cap12/fig12_2/Suma.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.mau.cap12.fig12_2;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author maureen
+ */
+public class Suma {
+    public static void main(String[] args) {
+        // obtiene la entrada del usuario de los diálogos de entrada de JOptionPane
+        String primerNumero = JOptionPane.showInputDialog("Introduzca el primer entero");
+        String segundoNumero = JOptionPane.showInputDialog("Introduzca el segundo entero");
+        
+        
+        
+    }
+}
