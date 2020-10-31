091e8e9da3c008608cee0221c4face9efd90d18b
Maureen Barahona <maureenbarahona@gmail.com>
2020-10-31 13:47:05 -0600

    convierte las entradas String en valores int para usarlos en un cálculo

--- src/main/java/hn/mau/cap12/fig12_2/Suma.java
+++ src/main/java/hn/mau/cap12/fig12_2/Suma.java
@@ -4,18 +4,23 @@
  * and open the template in the editor.
  */
 package hn.mau.cap12.fig12_2;
+
 import javax.swing.JOptionPane;
+
 /**
  *
  * @author maureen
  */
 public class Suma {
+
     public static void main(String[] args) {
         // obtiene la entrada del usuario de los diálogos de entrada de JOptionPane
         String primerNumero = JOptionPane.showInputDialog("Introduzca el primer entero");
         String segundoNumero = JOptionPane.showInputDialog("Introduzca el segundo entero");
-        
-        
-        
+
+        // convierte las entradas String en valores int para usarlos en un cálculo
+        int numero1 = Integer.parseInt(primerNumero);
+        int numero2 = Integer.parseInt(segundoNumero);
+
     }
 }
