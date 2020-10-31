a08537785cfc27eed1f8c9e533be478130e57b1a
Maureen Barahona <maureenbarahona@gmail.com>
2020-10-31 14:42:07 -0600

    Inicio fig 12_19

---
+++ src/main/java/hn/mau/cap12/fig12_19/PruebaBotonOpcion.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hn.mau.cap12.fig12_19;
+
+import javax.swing.JFrame;
+
+/**
+ *
+ * @author maureen
+ */
+public class PruebaBotonOpcion {
+
+    public static void main(String[] args) {
+        MarcoBotonOpcion marcoBotonOpcion = new MarcoBotonOpcion();
+        marcoBotonOpcion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        marcoBotonOpcion.setSize(300, 100);
+        marcoBotonOpcion.setVisible(true);
+    }
+}
